f8e1efbc2b96dd293b4b5cee965cd7866fe6c8ef
Joseph Clifford <jclifford4821@sdsu.edu>
2022-08-28 20:55:40 -0700

    starting inDanger

--- myAgents.py
+++ myAgents.py
@@ -1,4 +1,7 @@
-
+import game
+import ghostAgents
+import pacman
+import pacmanAgents
 from pacman import Directions
 from game import Agent, Actions
 from pacmanAgents import LeftTurnAgent
@@ -25,14 +28,82 @@ class TimidAgent(Agent):
         If the pacman is in danger we return the direction to the ghost.
         """
 
+
+
+
+
+        return Directions.STOP
+
+
+
+
+
         # Your code
         raise NotImplemented
     
     def getAction(self, state):
         """
         state - GameState
+
         
         Fill in appropriate documentation
         """
+        # List of directions the agent can choose from
+        legal = state.getLegalPacmanActions()
+        action = None
+        dir = None
 
-        raise NotImplemented
+        # Get the agent's state from the game state and find agent heading
+        pacmanState = state.getPacmanState()
+        pacmanPos = state.getPacmanPosition()       # Pacman state
+        pacX = pacmanPos[0]     # Pacman x position
+        pacY = pacmanPos[1]     # Pacman y position
+
+        heading = pacmanState.getDirection()
+
+        if heading == Directions.STOP:
+            # Pacman is stopped, assume North (true at beginning of game)
+            heading = Directions.NORTH
+
+
+        # List of Ghost states
+        ghostStates = pacman.GameState.getGhostStates(state)
+        ghost1 = ghostStates[0]     # Ghost1 state
+        ghost2 = ghostStates[1]     # Ghost2 state
+        g1X = ghost1.getPosition()[0]   # Ghost1 x position
+        g1Y = ghost1.getPosition()[1]   # Ghost2 y position
+        g2X = ghost2.getPosition()[0]   # Ghost1 x position
+        g2Y = ghost2.getPosition()[1]   # Ghost2 y position
+        action = None
+
+        for i in range(len(ghostStates)):
+            minDist = min(abs(pacX - g1X),abs(pacY - g1Y))
+            print(minDist)
+            if (pacX == g1X or pacY == g1Y) and (minDist <= 3):
+                dir = self.inDanger(self,pacmanState,ghost1)
+                print(dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # for ghostState in ghostStates:
+        #     print(len(ghostStates))
+        #     #print("{0} Index: {1}".format(ghostState.getPosition(), i))
+        #     #print(ghostState.getPosition())
+        #     print("Ghost 1: {0}\nGhost 2: {1}".format(ghostStates[0],ghostStates[1]))
+        #
+        #     i += 1
+        #     if not self.inDanger(self, pacmanState, ghostState):
+        #         return Directions.STOP
+
+
+        return pacmanAgents.LeftTurnAgent.getAction(self,state)
